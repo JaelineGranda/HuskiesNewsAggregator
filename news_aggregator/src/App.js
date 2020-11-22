821c15e368c47e9cc2d641a4b794f68da61b885b
charmiidalal <dalal.c@northeastern.edu>
2020-11-22 15:56:56 -0500

    Add weather api on homepage

--- news_aggregator/src/App.js
+++ news_aggregator/src/App.js
@@ -1,13 +1,66 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './index.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
+//Routing Links 
+class ToggleRoutes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onInputChangeSaveValue = this.onInputChangeSaveValue.bind(this)
+    }
+    onInputChangeSaveValue(e){
+        e.preventDefault();
+        var inputValue = e.target.value
+        this.props.displaySearchedNews(inputValue)
+    }
+    render(){
+        return(<nav id="main-nav">
+          <div class="container">
+            <img src="img/logo.png" alt="NewsMedia" class="logo"/>
+            <div class = 'social'>
+              <a href="http://facebook.com.br" target = 'blank'>Login</a>
+              <a href="http://twitter.com.br" target = 'blank'>Sign Up</a>
+            </div>
+            <ul>
+              <li><a href="index.html" class="current">Top News</a></li>
+              <li><a href="about.html" >Entertaiment</a></li>
+              <li><a href="about.html" >Sports</a></li>
+              <li><a href="about.html" >Business</a></li>
+              <li><a href="about.html" >World</a></li>
+            </ul>
+          </div>
+        </nav>
+        
+        )
+    }
+};
+
+//placeholder with hard height needed for indenting fixed nav bar
+function PlaceHolderForNavBar(){
+    return(
+        <div className="place-hold-nav"></div>
+    )
 }
+
+//parent of all pages, the top navbar stays same and still on all pages
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.displaySearchedNews = this.displaySearchedNews.bind(this)
+    }
+    displaySearchedNews(searchTextFromChild){
+        //routing to searched input
+        this.props.router.push('/news-search/'+ searchTextFromChild)
+    }
+    render(){
+        return(
+            <div>
+                <ToggleRoutes displaySearchedNews={this.displaySearchedNews} />
+                <PlaceHolderForNavBar />
+                {this.props.children}       
+            </div>
+        );
+    }
+}
+
 
 export default App;
---
+++ news_aggregator/src/general/Weather.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import createClass from 'create-react-class';
+//import $ from 'jquery';
+
+var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&zip='
+var urlTail = ',us&appid=0ea662971e6d6306ad9a84ede4d36865'
+
+//rendering weather, styling done here
+class Results extends Component {
+	render() {
+		var weather = this.props.propertyOfResultsNamedParentsState
+		var today = new Date();
+		var todayString = today.toDateString().slice(0, 10)
+		var windDirection = ''
+		// logic for wind direction
+		if (weather.wind > 45) {
+			windDirection = 'East'
+		} else if (weather.wind > 135) {
+			windDirection = 'South'
+		} else if (weather.wind > 225) {
+			windDirection = 'West'
+		} else { windDirection = 'North' }
+		return (
+			<div style={{ fontSize: 18, color: 'white', paddingTop: 10, paddingBottom: 10 }}>
+				<div style={{ position: 'relative' }}>
+					<div >
+						<div style={{ fontSize: 21 }}> {todayString} </div>
+						<div> {weather.name} </div>
+					</div>
+					<div style={{ width: 60, height: 60, margin: 'auto', textAlign: 'center' }}>
+						<img style={{ margin: 'auto', height: '100%', width: '100%' }} alt='' src={weather.iconUrl} />
+					</div>
+				</div>
+				<div style={{ marginBottom: 10 }}>
+					<div style={{ border: '3px solid white', display: 'inline-block', padding: 5, borderRadius: '50%', fontSize: 20 }}>{Math.floor(weather.temp)}&deg;</div>
+					<span style={{ fontSize: 20, marginLeft: 15, color: '#DD3939' }}>{Math.floor(weather.max)} &deg;</span> / <span style={{ fontSize: 18, color: '#0099FF' }}>{Math.floor(weather.min)} &deg;</span></div>
+				<div >{weather.description} </div>
+				<div> Humidty: {weather.humidity}% </div>
+				<div> Wind: {windDirection} at {Math.round(weather.windSpeed)} MPH</div>
+			</div>
+		)
+	}
+}
+
+// getJSON for weather data and passing it down as props
+var WeatherSearchSubmit = createClass({
+	getInitialState: function () {
+		return ({
+			temp: "", description: "", iconUrl: "",
+			zip: "", name: "", humidity: "", max: "",
+			min: "", wind: "", windSpeed: ""
+		})
+	},
+	componentDidMount: function () {
+		var url = weatherUrl + "02125" + urlTail
+		
+		fetch(url)
+			.then(res => res.json())
+			.then(
+				(weatherData) => {
+					this.setState({
+						temp: weatherData.main.temp,
+						iconUrl: 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + ".png",
+						description: weatherData.weather[0].description,
+						zip:this.props.input,
+						name: weatherData.name,
+						humidity: weatherData.main.humidity, 
+						max: weatherData.main.temp_max+1,
+						min: weatherData.main.temp_min-1,
+						wind: weatherData.wind.deg,
+						windSpeed: weatherData.wind.speed
+					});
+				},
+				// Note: it's important to handle errors here
+				// instead of a catch() block so that we don't swallow
+				// exceptions from actual bugs in components.
+				(error) => {
+					this.setState({
+						isLoaded: true,
+						error
+					});
+				}
+			)
+	},
+	weatherSearchSubmit: function (event) {
+		//getting weather data on submit of the weather input box
+		event.preventDefault();
+		var url = weatherUrl + event.target[0].value + urlTail;
+		fetch(url)
+			.then(res => res.json())
+			.then(
+				(weatherData) => {
+					this.setState({
+						temp: weatherData.main.temp,
+						iconUrl: 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + ".png",
+						description: weatherData.weather[0].description,
+						zip:this.props.input,
+						name: weatherData.name,
+						humidity: weatherData.main.humidity, 
+						max: weatherData.main.temp_max,
+						min: weatherData.main.temp_min,
+						wind: weatherData.wind.deg,
+						windSpeed: weatherData.wind.speed
+					});
+				},
+				// Note: it's important to handle errors here
+				// instead of a catch() block so that we don't swallow
+				// exceptions from actual bugs in components.
+				(error) => {
+					this.setState({
+						isLoaded: true,
+						error
+					});
+				}
+			)
+	},
+	render: function () {
+		//changing background animation depening on current weather.description
+		var weatherSource = './images/'
+		if(this.state.description.indexOf('cloud')>-1){
+			weatherSource+='cloud.png'
+		}else if(this.state.description.indexOf('mist')>-1){
+			weatherSource+='mist.png'
+		}else if(this.state.description.indexOf('rain')>-1){
+			weatherSource+='rain.png'
+		}else if(this.state.description.indexOf('snow')>-1){
+			weatherSource+='snow.png'
+		}else if(this.state.description.indexOf('thunder')>-1){
+			weatherSource+='thunder.png'}			
+		<img alt=' ' src={require('./images/mist.png')}/>
+		return (
+			<div style={{ paddingBottom: 10 }}>
+				<form onSubmit={this.weatherSearchSubmit} style={{ padding: 10 }}>
+					<input type="text" className='form-control' placeholder="Weather search by zip code" />
+				</form>
+				<div className='weather-move' style={{ position: 'absolute' }}></div>
+				<div className='weather-wrapper' style={{ textAlign: 'center', backgroundColor: '#2E2B31', width: '100%', margin: 'auto' }}>
+					<Results propertyOfResultsNamedParentsState={this.state} />
+
+				</div>
+			</div>
+		)
+	}
+})
+
+// weather boss
+var Weather = createClass({
+	render: function () {
+		return (
+			<WeatherSearchSubmit />
+		)
+	}
+})
+
+export default Weather 
