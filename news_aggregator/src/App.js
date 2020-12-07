047f887df03050626355131a77c0ecaee1b92985
DINESH KUMAR RAVIKUMAR <ravikumar.di@northeastern.edu>
2020-12-07 14:37:06 -0500

    added components

--- news_aggregator/src/App.js
+++ news_aggregator/src/App.js
@@ -3,9 +3,11 @@ import "./App.css";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import SingleNews from "./Components/NewsList/SingleNews/SingleNews";
 import Layout from "./Components/Menubar/Layout/Layout"
-import Footer from "./Components/Footer/Footer";
 import NewsList2 from "./Components/NewsList2/NewsList2";
 import Newslist from "./Components/NewsList/Newslist";
+import Login from './Components/login/login.component';
+import Signup from './Components/signup/signup.component';
+
 function App() {
   return (
     <div className="App">
@@ -13,16 +15,20 @@ function App() {
       <BrowserRouter basename={'NewsApp-using-React#'}>
       <Layout/>
         <Switch>
+        
         <Route exact path="/NewsApp-using-React/" render={() => (<Redirect to="#/Home" />)} />
           <Route path="/:category" exact component={NewsList2}></Route>
           <Route path="/:category/:id" component={SingleNews}></Route>
-        
+          <Route exact path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
         
           <Route exact path="/" render={() => (<Redirect to="/Home" />)} /> 
+          
+
         </Switch>
        
       </BrowserRouter>
-      <Footer/>
+      
     </div>
   );
 }
--- news_aggregator/src/Components/signup/signup.component.js
+++ news_aggregator/src/Components/signup/signup.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import '../login.css';
-
+import '../login/login.css';
 export default class SignUp extends Component {
     render() {
         return (
