0c875803aa6c5d47a2ade19cbe1e6d4c7569fe67
Jaeline Granda <granda.j@northeastern.edu>
2020-12-08 20:11:10 -0500

    Reducers update state based on dispatched redux actions

---
+++ news_aggregator/src/reducers/auth.js
@@ -0,0 +1,52 @@
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT,
+  } from "../actions/types";
+  
+  // gets user item stored in local storage
+  const user = JSON.parse(localStorage.getItem("user"));
+  
+  // checks if initial state of user is logged in or not
+  const initialState = user
+    ? { isLoggedIn: true, user }
+    : { isLoggedIn: false, user: null };
+  
+  export default function (state = initialState, action) {
+    const { type, payload } = action;
+    // updates state based on redux actions
+    switch (type) {
+      case REGISTER_SUCCESS:
+        return {
+          ...state,
+          isLoggedIn: false,
+        };
+      case REGISTER_FAIL:
+        return {
+          ...state,
+          isLoggedIn: false,
+        };
+      case LOGIN_SUCCESS:
+        return {
+          ...state,
+          isLoggedIn: true,
+          user: payload.user,
+        };
+      case LOGIN_FAIL:
+        return {
+          ...state,
+          isLoggedIn: false,
+          user: null,
+        };
+      case LOGOUT:
+        return {
+          ...state,
+          isLoggedIn: false,
+          user: null,
+        };
+      default:
+        return state;
+    }
+  }
