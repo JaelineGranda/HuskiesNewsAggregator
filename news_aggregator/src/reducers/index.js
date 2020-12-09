1817d62526de91359d74307c6bbcbd08658028ec
Jaeline Granda <granda.j@northeastern.edu>
2020-12-08 20:21:08 -0500

    Redux store that brings actions/reducers together and holds app state

--- news_aggregator/src/reducers/index.js
+++ news_aggregator/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import auth from "./auth";
 import message from "./message";
 
+// reducer composition
 export default combineReducers({
   auth,
   message,
