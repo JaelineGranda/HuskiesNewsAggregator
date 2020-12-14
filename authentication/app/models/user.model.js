afe4d359a637274b2926d51f7cef37f0fa2824d6
Jaeline Granda <granda.j@northeastern.edu>
2020-12-13 19:21:22 -0500

    Undo changes for mongoose schema

--- authentication/app/models/user.model.js
+++ authentication/app/models/user.model.js
@@ -6,19 +6,12 @@ const User = mongoose.model(
     username: String,
     email: String,
     password: String,
-    countries: {
-      type: Array,
-      default: ["United States of America", "United Kingdom", "Canada", "China", "Russia", "France",
-                "Philippines", "United Arab Emirates", "Australia", "Argentina", "South Korea", "Indonesia"]
-      },
-    categories: {
-      type: Array,
-      default: ["Business", "Entertainment", "General", "Health", "Science", "Sports", "Technology"]
-    },
-    bookmarks: {
-      type: Array,
-      default: []
-    }
+    roles: [ // User object will have a roles array that contains ids as reference
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role"
+      }
+    ]
   })
 );
 
