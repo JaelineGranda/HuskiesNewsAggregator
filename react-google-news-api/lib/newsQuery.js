b50bb3444cf611323e9457afccff1756485f2439
charmiidalal <dalal.c@northeastern.edu>
2020-12-15 09:53:46 -0500

    Add gitignore files

--- react-google-news-api/lib/newsQuery.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  sourceByTop(source) {
-    return 'https://newsapi.org/v2/top-headlines?sources='
-      + source
-      + '&apiKey='
-      + process.env.API_KEY; //set API key via environment or dotenv file
-  }
-}
