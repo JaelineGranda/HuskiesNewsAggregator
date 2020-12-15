868127a318cb2b1b7799c11eb453dcc42e9d418b
Jaeline Granda <granda.j@northeastern.edu>
2020-12-14 21:59:22 -0500

    Add video page with youtube api

---
+++ react-news-search-and-bookmark-master/src/components/video/video.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import SearchBar from './searchbar/searchbar';
+import { config } from './api/youtubeapi';
+import VideoList from './videolist';
+import VideoDetail from './videodetail';
+import axios from 'axios';
+import './video.component.css'
+
+class Video extends React.Component {
+    state = {
+        videos: [],
+        selectedVideo: null
+    }
+    handleSubmit = async (termFromSearchBar) => {
+        config['params'] = { ...config['params'], q: termFromSearchBar};
+        const response = await axios.get('/search', config);
+
+        this.setState({
+            videos: response.data.items
+        })
+        console.log("this is resp",response);
+    };
+    handleVideoSelect = (video) => {
+        this.setState({selectedVideo: video})
+    }
+
+    render() {
+        return (
+            <div className='ui container' style={{marginTop: '1em'}}>
+                <SearchBar handleFormSubmit={this.handleSubmit}/>
+                <div className='ui grid'>
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo}/>
+                        </div>
+                        <div className="five wide column">
+                            <VideoList handleVideoSelect={this.handleVideoSelect} videos={this.state.videos}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Video;
